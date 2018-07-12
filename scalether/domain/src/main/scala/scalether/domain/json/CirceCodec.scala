c01ec0e48b4ada6f787cff4d8de1e4c3da5d6115
Hugh Giddens <hgiddens@gmail.com>
2018-07-12 14:09:26 +0200

    Documentation tweaked.

--- scalether/domain/src/main/scala/scalether/domain/json/CirceCodec.scala
+++ scalether/domain/src/main/scala/scalether/domain/json/CirceCodec.scala
@@ -6,6 +6,12 @@ import cats.implicits._
 import io.circe.{Decoder, DecodingFailure, Encoder}
 import io.circe.syntax._
 
+/**
+  * Implicits that allow [[BigInteger]] values to be serialised and deserialised as expected.
+  * <p>
+  * These implicits <i>must</i> be visible in any contexts where [[BigInteger]] values are sent
+  * to or received from an Ethereum network.
+  */
 object CirceCodec {
   // This is the hacky part; Circe already defines a Decoder and Encoder for BigInteger
   // Because it's found via companion search we can override it by providing our own imlementation
@@ -13,8 +19,8 @@ object CirceCodec {
   // obviously be a fairly intrusive change though.
   implicit def bigIntegerDecoder: Decoder[BigInteger] =
     Decoder.instance { c =>
-      // See BigIntegerHexDeserializer
-      // This doesn't have the single value array decoding stuff as it doesn't appear to be used.
+      // This doesn't have the single value array decoding stuff that was in
+      // BigIntegerHexDeserializer, as it doesn't appear to ever be used.
       val asNumber = Decoder.decodeJavaBigInteger(c)
       val asString = c.as[String].flatMap { text =>
         try {

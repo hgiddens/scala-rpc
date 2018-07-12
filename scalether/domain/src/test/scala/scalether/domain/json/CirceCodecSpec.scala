029ff7bf06509845aefc7b428c8b0400feecfac9
Hugh Giddens <hgiddens@gmail.com>
2018-07-12 14:05:49 +0200

    Test codec for lawfulness.

--- scalether/domain/src/test/scala/scalether/domain/json/CirceCodecSpec.scala
+++ scalether/domain/src/test/scala/scalether/domain/json/CirceCodecSpec.scala
@@ -5,9 +5,17 @@ import java.math.BigInteger
 import io.circe.parser.parse
 import io.circe.syntax._
 import org.scalatest.FlatSpec
+import org.scalatest.prop.GeneratorDrivenPropertyChecks._
 import scalether.domain.json.CirceCodec._
 
 final class CirceCodecSpec extends FlatSpec {
+  "codec" should "be lawful" in {
+    forAll { bigInt: BigInt =>
+      val bigInteger = bigInt.bigInteger
+      assert(bigInteger.asJson.as[BigInteger] == Right(bigInteger))
+    }
+  }
+
   "decoder" should "deserialise BigIntegers" in {
     val expected = new BigInteger("20000000000")
     assert(parse("\"0x4A817C800\"").right.flatMap(_.as[BigInteger]) == Right(expected))
